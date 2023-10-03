a1b69b38afc3ef2e2b3ccb197311ba4ad5b73e7b
torchdotonions <114874349+torchdotonions@users.noreply.github.com>
2023-10-04 00:47:55 +0530

    Create fibonacci.py

---
+++ fibonacci.py
@@ -0,0 +1,8 @@
+def fibonacci(n):
+    fib = [0, 1] + [0] * (n - 1)
+    for i in range(2, n + 1):
+        fib[i] = fib[i - 1] + fib[i - 2]
+    return fib[n]
+
+result = fibonacci(10)
+print(result)  # Output: 55
